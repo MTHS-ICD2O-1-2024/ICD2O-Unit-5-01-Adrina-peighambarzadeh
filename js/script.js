7944ada68b17a2c0254eab9d5cf5cc7f54b66a84
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-04-15 17:16:58 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Adrina peighambarzadeh All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adrina peighambarzadeh
+
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+* This function calculates the area of a triangle.
+*/
+function calculateConvert() {
+  // input
+
+  const dgreeInFahrenheit = parseFloat(
+    document.getElementById("dgree-in-fahrenheit").value
+  )
+
+  // process
+  const ConvertedToCelsius =
+    (dgreeInFahrenheit - 32) * (5 / 9)
+
+  // output
+  document.getElementById("answer").innerHTML =
+    "your dgree in celsius is: " + ConvertedToCelsius.toFixed(3) + "°C"
+}
